6bb7aa86a61861ba88e17bba589ceb26ad0ed5ce
ashu <207ashu@gmail.com>
2018-05-02 21:21:28 +0530

    videos playing in a sequence and loop

--- app/src/main/java/com/example/root/videoview/Main2Activity.java
+++ app/src/main/java/com/example/root/videoview/Main2Activity.java
@@ -1,13 +1,61 @@
 package com.example.root.videoview;
 
+import android.app.Dialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.VideoView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    VideoView videoView;
+    Dialog videoDialog;
+
+    static String name[]={"video_one","video_two","video_three"};
+    static int index=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        videoDialog = new Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+        videoDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        videoDialog.setContentView(R.layout.video_fragment);
+        // videoDialog.setOnKeyListener();
+        videoView = (VideoView) videoDialog.findViewById(R.id.videoView2);
+        videoDialog.show();
+        videoView.requestFocus();
+
+        videoView.setVideoPath(getPath(index));
+        index++;
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoView.start();
+            }
+        });
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                if (index == name.length)
+                    index = 0;
+
+                videoView.setVideoPath(getPath(index));
+                index++;
+            }
+        });
+
+    
+        }
+    private String getPath(int id) {
+        return "android.resource://com.example.root.videoview/raw/" + name[id];
     }
-}
+        
+    }
+
+
--- app/src/main/java/com/example/root/videoview/MainActivity.java
+++ app/src/main/java/com/example/root/videoview/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.root.videoview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+    Button stopBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoView=findViewById(R.id.videoView);
+        stopBtn=findViewById(R.id.stop_button);
+
+
+
     }
 }
